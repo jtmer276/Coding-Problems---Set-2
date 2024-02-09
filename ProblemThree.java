87390749586c544e20fb44333344fcc31eb9f7d4
jtmer276 <jtmerrill276@gmail.com>
2024-02-09 14:47:45 -0500

    Update ProblemThree.java

--- ProblemThree.java
+++ ProblemThree.java
@@ -7,7 +7,7 @@ Date : February 9th, 2024
 public class ProblemThree {
 
     
-    public static void main(String[], args ){
+    public static void main(String[]args ){
         System.out.println("\t Anderson \t 3452");
         System.out.println("\t Boyton \t 5344");
         System.out.println("\t Carrington \t 3677");
